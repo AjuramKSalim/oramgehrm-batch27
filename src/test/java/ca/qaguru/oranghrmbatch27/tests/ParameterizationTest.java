7f499c77bb5e78d738dd22937e452e6c8015b335
Aju <ajusalimsays143@gmail.com>
2023-01-22 08:20:09 -0500

    commit changes

--- src/test/java/ca/qaguru/oranghrmbatch27/tests/ParameterizationTest.java
+++ src/test/java/ca/qaguru/oranghrmbatch27/tests/ParameterizationTest.java
@@ -3,7 +3,7 @@ package ca.qaguru.oranghrmbatch27.tests;
 import org.testng.annotations.Test;
 
 public class ParameterizationTest {
-    @Test(dataProvider = "LoginInfo", dataProviderClass = ca.qaguru.oranghrmbatch21.tests.ParameterizationTestData.class)
+    @Test(dataProvider = "LoginInfo", dataProviderClass = ca.qaguru.oranghrmbatch27.tests.ParameterizationTestData.class)
     public void sampleTest(String username, String password){
         System.out.println("Username : "+ username + " Password: "+ password);
     }
