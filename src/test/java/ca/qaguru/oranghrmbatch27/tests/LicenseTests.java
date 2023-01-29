e3d8e2490d7fb67f2736d943657abd12337ff462
BDK-1990 <bharath.dinesh@ymail.com>
2023-01-29 10:51:04 -0500

    Test case to add License.

---
+++ src/test/java/ca/qaguru/oranghrmbatch27/tests/LicenseTests.java
@@ -0,0 +1,21 @@
+package ca.qaguru.oranghrmbatch27.tests;
+
+import ca.qaguru.oranghrmbatch27.library.TestBase;
+import ca.qaguru.oranghrmbatch27.pages.*;
+import org.testng.annotations.Test;
+
+import java.util.UUID;
+
+public class LicenseTests extends TestBase {
+    @Test
+    public void addNewLicense() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("Admin", "admin123", true, null);
+        HeaderPage headerPage = new HeaderPage(driver);
+        headerPage.selectMenu(MenuOptions.LICENSES);
+        LicensePage licensePage = new LicensePage(driver);
+        licensePage.saveNewLicense("ISO-2005");
+       // String uuid = UUID.randomUUID().toString();
+      //  licensePage.saveNewLicense("ISO-2005" + uuid);
+    }
+}
