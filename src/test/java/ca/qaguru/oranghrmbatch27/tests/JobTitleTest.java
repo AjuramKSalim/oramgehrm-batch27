bd1ef4d01f20f5ba6367eb9c2eb1a9ddeb2e97a5
Neethujn <neethujn19@gmail.com>
2023-02-05 09:59:24 -0500

    - removed the blank line(from jobtestpage,jobtitlepage) -renamed the method from saveNewJob to saveJobAndVerifyDisplayed -removed text id from variable name(Job Page)

--- src/test/java/ca/qaguru/oranghrmbatch27/tests/JobTitleTest.java
+++ src/test/java/ca/qaguru/oranghrmbatch27/tests/JobTitleTest.java
@@ -1,26 +1,20 @@
 package ca.qaguru.oranghrmbatch27.tests;
-
 import ca.qaguru.oranghrmbatch27.library.TestBase;
 import ca.qaguru.oranghrmbatch27.pages.*;
 import org.testng.annotations.Test;
-
 import java.util.UUID;
-
 public class JobTitleTest extends TestBase {
-
     @Test
     public void addNewJob( ) {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login("admin", "admin123", true, null);
         HeaderPage headerPage = new HeaderPage(driver);
         headerPage.selectMenu(MenuOptions.JOB_TITLES);
-
-
         JobTitlePage jobPage = new JobTitlePage(driver);
-        jobPage.saveNewJob("Quality Engineer");
-       // String uuid = UUID.randomUUID().toString();
-        //String jobname= "JobTitle Test " + uuid;
-       // jobPage.saveNewJob(jobname);
+        jobPage.saveJobAndVerifyDisplayed("Quality Engineer");
+        String uuid = UUID.randomUUID().toString();
+        String jobname= "JobTitle Test " + uuid;
+        jobPage.saveJobAndVerifyDisplayed(jobname);
     }
 
 }
