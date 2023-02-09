a5b6ce2d71423f70b9eaa3e5a98078a15f76262b
Neethujn <neethujn19@gmail.com>
2023-02-08 21:43:55 -0500

    Bt-10 Testcase For Delete a Job

--- src/test/java/ca/qaguru/oranghrmbatch27/pages/JobTitlePage.java
+++ src/test/java/ca/qaguru/oranghrmbatch27/pages/JobTitlePage.java
@@ -1,62 +1,78 @@
 package ca.qaguru.oranghrmbatch27.pages;
-
 import ca.qaguru.oranghrmbatch27.library.PageBase;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
 import java.util.List;
 
 public class JobTitlePage extends PageBase {
-
     WebDriver driver;
-    private final String idJobAddBtn = ".oxd-button--secondary";
-    private final String idJobTxtLevel="div[class='oxd-form-row'] input[class*='oxd-input']";
-    private final String idJobSaveBtn="//button[@type='submit']";
 
+    private final String jobAddBtn = ".oxd-button--secondary";
+    private final String jobTxtLevel="div[class='oxd-form-row'] input[class*='oxd-input']";
+    private final String jobSaveBtn="//button[@type='submit']";
     private final String lblAlreadyExistsMessage = "//div[@class='oxd-form-row'] /div";
-    private final String getIdJobCancelBtn= "//div[@class='oxd-form-actions'] /button[1]";
+    private final String jobCancelBtn= "//div[@class='oxd-form-actions'] /button[1]";
     private final String tblJob = ".oxd-table-body";
-    //private final String educations = "//div[@class='oxd-table-body'] /div[@class='oxd-table-card']";
-
     private final String jobs=" //div[contains(@class, 'oxd-table-body')]/div[contains(@class,'oxd-table-card')]";
-    private final  String deleteButton="//div[contains(@class, 'oxd-table-cell-actions')]/button[1]";
+
     @FindBy(xpath = jobs)
     private List<WebElement> listJobs;
-
 
     public JobTitlePage(WebDriver driver) {
         super(driver);
         this.driver=driver;
+
         PageFactory.initElements(driver,this);
     }
-//Code for add a new Job title to the list
-    public void saveNewJob(String jobName) {
-        click(By.cssSelector(idJobAddBtn));
-        setText(By.cssSelector(idJobTxtLevel), jobName);
+    public void saveJobAndVerifyDisplayed(String jobName) {
+        click(By.cssSelector(jobAddBtn));
+        setText(By.cssSelector(jobTxtLevel), jobName);
         if (getText(By.xpath(lblAlreadyExistsMessage)).contains("Already exists")) {
-            click(By.xpath(getIdJobCancelBtn));
+            click(By.xpath(jobCancelBtn));
             System.out.println("The Job title is already exsts");
         } else {
-            click(By.xpath(idJobSaveBtn));
+            click(By.xpath(jobSaveBtn));
         }
         isElementVisible(By.cssSelector(tblJob) );
 
         for (WebElement Job : listJobs) {
             String txtEducation = Job.getText();
             System.out.println(txtEducation);
-
         }
-
-        //Checking the added job title is listing
         Boolean match = listJobs.stream().map(s -> s.getText()).anyMatch(s -> s.equalsIgnoreCase(jobName));
         Assert.assertTrue(match);
         System.out.println("\n");
         System.out.println(jobName + " is added successfully");
         System.out.println("\n");
+    }
 
-    }
-    }
+    public void deleteAndVerifyJob(String jobName) {
+        isElementVisible(By.cssSelector(tblJob));
+        for (WebElement Job : listJobs) {
+            String txtEducation = Job.getText();
+            if (txtEducation.equalsIgnoreCase(jobName)){
+                System.out.println(txtEducation+" is available");
+                clickDeleteIcon(jobName);
+                break;
+            }
+            }
+        Boolean match = listJobs.stream().map(s -> s.getText()).anyMatch(s -> s.equalsIgnoreCase(jobName));
+        Assert.assertFalse(match);
+        System.out.println("\n");
+        System.out.println(jobName + " is deleted successfully");
+        System.out.println("\n");
+        }
+
+    public void clickDeleteIcon(String jname){
+        String btnxpath="//div[@class='oxd-table-row oxd-table-row--with-border' and .//div[text()='"+jname+"']]//button[1]";
+        click(By.xpath(btnxpath));
+        click(By.xpath("//*[@id='app']/div[3]/div/div/div/div[3]/button[2]"));
+      }
+}
--- src/test/java/ca/qaguru/oranghrmbatch27/tests/JobTitleTest.java
+++ src/test/java/ca/qaguru/oranghrmbatch27/tests/JobTitleTest.java
@@ -1,26 +1,33 @@
 package ca.qaguru.oranghrmbatch27.tests;
-
 import ca.qaguru.oranghrmbatch27.library.TestBase;
 import ca.qaguru.oranghrmbatch27.pages.*;
 import org.testng.annotations.Test;
-
 import java.util.UUID;
-
 public class JobTitleTest extends TestBase {
 
+    String jobName="Tesing001";
     @Test
     public void addNewJob( ) {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login("admin", "admin123", true, null);
         HeaderPage headerPage = new HeaderPage(driver);
         headerPage.selectMenu(MenuOptions.JOB_TITLES);
-
-
         JobTitlePage jobPage = new JobTitlePage(driver);
-        jobPage.saveNewJob("Quality Engineer");
-       // String uuid = UUID.randomUUID().toString();
-        //String jobname= "JobTitle Test " + uuid;
-       // jobPage.saveNewJob(jobname);
+        jobPage.saveJobAndVerifyDisplayed(jobName);
+        String uuid = UUID.randomUUID().toString();
+        String jobname1= jobName + uuid;
+        jobPage.saveJobAndVerifyDisplayed(jobname1);
     }
+    @Test
+    public  void deleteJob(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("admin", "admin123", true, null);
+        HeaderPage headerPage = new HeaderPage(driver);
+        headerPage.selectMenu(MenuOptions.JOB_TITLES);
+        JobTitlePage jobPage = new JobTitlePage(driver);
+        jobPage.deleteAndVerifyJob(jobName);
+
+    }
+
 
 }
