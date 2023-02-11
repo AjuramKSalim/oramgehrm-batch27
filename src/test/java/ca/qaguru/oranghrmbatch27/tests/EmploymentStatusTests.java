47f00557439ab5b8de4df64c6f68df31537666e4
preethizcm <123276410+preethizcm@users.noreply.github.com>
2023-02-11 00:18:06 -0500

    Review comment updates

--- src/test/java/ca/qaguru/oranghrmbatch27/tests/EmploymentStatusTests.java
+++ src/test/java/ca/qaguru/oranghrmbatch27/tests/EmploymentStatusTests.java
@@ -1,19 +1,16 @@
 package ca.qaguru.oranghrmbatch27.tests;
-
 import ca.qaguru.oranghrmbatch27.library.TestBase;
 import ca.qaguru.oranghrmbatch27.pages.*;
 import org.testng.annotations.Test;
-
 public class EmploymentStatusTests extends TestBase {
 
-
     @Test
-    public void addNewEmploymentStatus() {
+    public void addNewEmploymentAndVerifyIsDisplayed() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login("admin", "admin123", true, null);
         HeaderPage headerPage = new HeaderPage(driver);
         headerPage.selectMenu(MenuOptions.EMPLOYMENT_STATUS);
         EmploymentStatusPage employmentStatusPage = new EmploymentStatusPage(driver);
-        employmentStatusPage.addEmploymentStatus("Freelance2");
+        employmentStatusPage.addEmploymentStatus("Freelance" + uuid.toString());
     }
 }
